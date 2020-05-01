230989d4d939ff25c7e659180173b4851992e184
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2020-05-01 10:16:50 +0200

    Adds more requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['flask', 'flask-sqlalchemy', 'sqlalchemy']
+requirements = ['flask', 'flask-sqlalchemy', 'sqlalchemy', 'mysqlclient']
 
 setup_requirements = ['pytest-runner', ]
 
