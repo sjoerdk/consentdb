722571d11246b1e348b39ade18401b5530c248f0
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2020-05-01 10:16:54 +0200

    Bump version: 0.2.2 → 0.2.3

--- consentdb/__init__.py
+++ consentdb/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for Consent database."""
 __author__ = """Sjoerd Kerkstra"""
 __email__ = 'sjoerd.kerkstra@radboudumc.nl'
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 from os import environ
 from flask import Flask
